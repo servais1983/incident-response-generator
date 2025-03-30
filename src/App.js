24dc7489224f775824e5928a931d85f006f848f9
servais1983 <stservais0409@gmail.com>
2025-03-30 22:01:44 +0200

    Ajouter fichier App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IncidentResponseGenerator from './components/IncidentResponseGenerator';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <IncidentResponseGenerator />
+    </div>
+  );
+}
+
+export default App;
